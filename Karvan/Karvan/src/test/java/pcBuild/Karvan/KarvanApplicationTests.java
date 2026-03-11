1ca5ff68e472a6e7e99a7f6b44a3575044117e08
Mitilll <frazaosilva90@gmail.com>
2026-03-11 10:41:23 -0300

    Arquitetura: estruturacao MVC (Service, Model, Repository e Controller

---
+++ Karvan/Karvan/src/test/java/pcBuild/Karvan/KarvanApplicationTests.java
@@ -0,0 +1,13 @@
+package pcBuild.Karvan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarvanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
